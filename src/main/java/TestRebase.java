e2a57a1080a26b619360cd3ee0bf8f1f4d0c1418
mobei <mobei615@foxmail.com>
2022-05-30 22:17:30 +0800

    分支1-任务1

--- src/main/java/TestRebase.java
+++ src/main/java/TestRebase.java
@@ -6,6 +6,7 @@
 public class TestRebase {
     public static void main(String[] args) {
         System.out.println("------master------");
+        System.out.println("------branch 1 task1------");
         System.out.println("------branch 2 task2------");
         System.out.println("------branch 2 task3------");
     }
